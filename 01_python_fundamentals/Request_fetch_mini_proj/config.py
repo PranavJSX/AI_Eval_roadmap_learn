ad3de3b41a3061d938319b94dcd7a8863b895cd4
manuo <ojhapranav1999@gmail.com>
2026-08-06 10:13:47 +0530

    Creating config, writing client function and classes for the asnycio mini project

---
+++ 01_python_fundamentals/Request_fetch_mini_proj/config.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class settings:
+    BASE_URL: str = "https://jsonplaceholder.typicode.com"
+    MAX_CONCURRENT_REQUESTS: int = 5
+    TIMEOUT: int = 10  # Timeout in seconds for HTTP requests
+    MAX_KEEPALIVE_CONNECTIONS: int = 10  # Maximum number of keep-alive connections
+
+settings = Settings()
