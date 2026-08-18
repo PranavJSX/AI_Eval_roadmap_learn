17c1f1fcb113da6769c4974834de680e2fb791e9
manuo <ojhapranav1999@gmail.com>
2026-08-18 13:26:21 +0530

    Pushing the changes for prac.py files while learning the decorators

---
+++ _python_fundamentals/Python_decorators/prac.py
@@ -0,0 +1,46 @@
+def parent(num):
+
+    print("Executing parent function")
+
+    def inner1():
+        print('Executing from inner 1')
+        return None
+
+    def inner2():
+        print('Executing from inner 2')
+        return None
+
+    if num == 1:
+        return inner1
+
+    else:
+        return inner2
+
+def decorator1(func):
+    def wrapper():
+        print("Something is happening before the function")
+        func()
+        print("Something is happening after the function")
+    return wrapper
+
+#using pie syntax to use decorator
+@decorator1
+def say_whee():
+    print("WHEEE!!")
+
+
+
+# say_whee = decorator1(say_whee)
+def main():
+    # parent()
+
+    # first = parent(1)
+    # second  = parent(2)
+
+    # print(first())
+    # print(second())=
+
+    print(say_whee())
+
+if __name__=="__main__":
+    main()
