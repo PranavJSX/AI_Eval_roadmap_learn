83a992d0499770e52cfb4afe57eed04f9b5c5c04
manuo <ojhapranav1999@gmail.com>
2026-08-24 14:41:19 +0530

    adding pytest prac and fixtures along with marks

---
+++ 02_python_libraries/Pytest/fixture.py
@@ -0,0 +1,43 @@
+import pytest
+
+@pytest.fixture
+def example_fixture():
+    return 1
+
+# @pytest.fixture
+def format_data_for_display(people):
+    print('Printing people array here',people)
+    temp = []
+    for data in people:
+        name = data['given_name']+ ' '+ data['family_name']
+        dict1 = f"{name}: {data['title']}"
+        temp.append(dict1)
+
+    return temp
+
+@pytest.fixture
+def example_people_data():
+    return [
+        {
+            "given_name":"Alfonsa",
+            "family_name":"Ruiz",
+            "title":"Senior Software Engineer"
+        },
+        {
+            "given_name": "Sayid",
+            "family_name": "Khan",
+            "title": "Project Manager",
+        },
+    ]
+
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+    )
+
+
+@pytest.fixture
+def cmdopt(request):
+    return request.config.getoption("--cmdopt")
--- 02_python_libraries/Pytest/test_prac1.py
+++ 02_python_libraries/Pytest/test_prac1.py
@@ -1,3 +1,5 @@
+from fixture import *
+
 def test_always_passes():
     assert True
 
@@ -13,7 +15,6 @@ def test_uppercase():
 def test_reversed():
     assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
 
-
 def test_some_prime():
     assert 37 in {
         num for num in range(2, 50) if not any(num % div == 0 for div in range(2, num))
@@ -21,3 +22,24 @@ def test_some_prime():
 
 def main():
     pass
+
+
+def test_with_fixture(example_fixture):
+    assert example_fixture == 1
+
+
+# TDD, writing a test to verify if an api returns user data as expected
+
+def test_format_data_for_display(example_people_data):
+    assert format_data_for_display(example_people_data) == [
+        "Alfonsa Ruiz: Senior Software Engineer",
+        "Sayid Khan: Project Manager",
+    ]
+
+
+@pytest.mark.skip()
+def test_wtr_command_line_option(cmdopt):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
